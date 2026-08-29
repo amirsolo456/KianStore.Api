c5bcc3e6228657ca3c6feaa4446b16fc39d74474
Amir <amirsol456@gmail.com>
2026-08-30 02:43:14 +0330

    Align DbContext with core KianStore entities

--- Data/KianStoreDbContext.cs
+++ Data/KianStoreDbContext.cs
@@ -1,5 +1,5 @@
-using Microsoft.EntityFrameworkCore;
 using KianStore.Api.Models.KianStore;
+using Microsoft.EntityFrameworkCore;
 
 namespace KianStore.Api.Data;
 
@@ -10,19 +10,16 @@ public class KianStoreDbContext : DbContext
     {
     }
 
-    public DbSet<Kala> Kalas { get; set; }
-    public DbSet<Taraf> Tarafs { get; set; }
-    public DbSet<Sanad> Sanads { get; set; }
-    public DbSet<SanadDetail> SanadDetails { get; set; }
+    public DbSet<Kala> Kalas => Set<Kala>();
+    public DbSet<KalaDetail> KalaDetails => Set<KalaDetail>();
+    public DbSet<Taraf> Tarafs => Set<Taraf>();
+    public DbSet<Anbar> Anbars => Set<Anbar>();
+    public DbSet<Sanad> Sanads => Set<Sanad>();
+    public DbSet<SanadDetail> SanadDetails => Set<SanadDetail>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
-
-        modelBuilder.Entity<Taraf>(entity =>
-        {
-            entity.HasKey(x => new { x.Id, x.IdType });
-        });
 
         modelBuilder.Entity<Kala>(entity =>
         {
@@ -31,18 +28,47 @@ public class KianStoreDbContext : DbContext
             entity.Property(x => x.MabKharid).HasPrecision(18, 3);
         });
 
-        modelBuilder.Entity<Sanad>(entity =>
+        modelBuilder.Entity<KalaDetail>(entity =>
+        {
+            entity.HasKey(x => new { x.IdKala, x.IdAnbar });
+            entity.Property(x => x.LastMabKharid).HasPrecision(18, 3);
+            entity.Property(x => x.MabFrosh).HasPrecision(18, 3);
+            entity.Property(x => x.MabFrosh1).HasPrecision(18, 3);
+        });
+
+        modelBuilder.Entity<Taraf>(entity =>
+        {
+            entity.HasKey(x => new { x.Id, x.IdType });
+        });
+
+        modelBuilder.Entity<Anbar>(entity =>
         {
             entity.HasKey(x => x.Id);
-            entity.Property(x => x.TotalAmount).HasPrecision(18, 3);
+        });
+
+        modelBuilder.Entity<Sanad>(entity =>
+        {
+            entity.HasKey(x => new { x.IdSal, x.Id });
+            entity.Property(x => x.MabKol).HasPrecision(18, 3);
+            entity.Property(x => x.MabNaghd).HasPrecision(18, 3);
+            entity.Property(x => x.MabFrosh).HasPrecision(18, 3);
+            entity.Property(x => x.Takhfif).HasPrecision(18, 3);
+            entity.Property(x => x.MabDarSad).HasPrecision(18, 3);
         });
 
         modelBuilder.Entity<SanadDetail>(entity =>
         {
-            entity.HasKey(x => new { x.Id, x.IdKala });
-            entity.Property(x => x.Quantity).HasPrecision(18, 3);
-            entity.Property(x => x.UnitPrice).HasPrecision(18, 3);
-            entity.Property(x => x.TotalPrice).HasPrecision(18, 3);
+            entity.HasKey(x => new { x.IdSal, x.IdSanad, x.Id2 });
+            entity.Property(x => x.BedMab).HasPrecision(18, 5);
+            entity.Property(x => x.BesMab).HasPrecision(18, 5);
+            entity.Property(x => x.SumMab).HasPrecision(18, 3);
+            entity.Property(x => x.BedMabKharid).HasPrecision(18, 3);
+            entity.Property(x => x.Maliat).HasPrecision(18, 3);
+            entity.Property(x => x.HazKala).HasPrecision(18, 3);
+            entity.Property(x => x.HazKalaKharid).HasPrecision(18, 3);
+            entity.Property(x => x.BedMab2).HasPrecision(18, 5);
+            entity.Property(x => x.BesMab2).HasPrecision(18, 5);
+            entity.Property(x => x.SumTakhfifKala).HasPrecision(18, 3);
         });
     }
 }
