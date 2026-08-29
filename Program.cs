e862e49d52c2079542dab265524f1e1433087b10
Amir <amirsol456@gmail.com>
2026-08-30 02:24:28 +0330

    Initialize mobile order tables at startup

--- Program.cs
+++ Program.cs
@@ -32,6 +32,12 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<KianStoreDbContext>();
+    await MobileOrderDatabaseInitializer.InitializeAsync(dbContext);
+}
+
 app.UseMiddleware<GlobalExceptionMiddleware>();
 
 if (app.Environment.IsDevelopment())
