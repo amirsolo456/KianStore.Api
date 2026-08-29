6bd5462ca139716698dde09760b170e6d51df9fd
Amir <amirsol456@gmail.com>
2026-08-30 02:45:58 +0330

    Add document service contract

---
+++ Services/Interfaces/IDocumentService.cs
@@ -0,0 +1,16 @@
+using KianStore.Api.Common;
+using KianStore.Api.DTOs.Documents;
+
+namespace KianStore.Api.Services.Interfaces;
+
+public interface IDocumentService
+{
+    Task<ApiResponse<DocumentResponse>> CreateAsync(
+        CreateDocumentRequest request,
+        CancellationToken cancellationToken = default);
+
+    Task<ApiResponse<DocumentResponse>> GetAsync(
+        int idSal,
+        string id,
+        CancellationToken cancellationToken = default);
+}
