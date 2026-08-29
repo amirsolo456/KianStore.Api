f2bda7a6cd32cfd5c0acad1a8afc9b91676c4888
Amir <amirsol456@gmail.com>
2026-08-30 02:35:02 +0330

    Remove obsolete mobile-order Sanad conversion interface

--- Services/Interfaces/ISanadService.cs
+++
@@ -1,9 +0,0 @@
-using KianStore.Api.Common;
-using KianStore.Api.Models.Orders;
-
-namespace KianStore.Api.Services.Interfaces;
-
-public interface ISanadService
-{
-    Task<ApiResponse<string>> ConvertToSanadAsync(long mobileOrderId);
-}
