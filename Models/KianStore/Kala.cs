e9d34de86e2f5befff55ba2c2ed88824006d07c0
Amir <amirsol456@gmail.com>
2026-08-30 02:46:27 +0330

    Align Kala model with KianStore schema

--- Models/KianStore/Kala.cs
+++ Models/KianStore/Kala.cs
@@ -15,6 +15,12 @@ public class Kala
     [StringLength(50)]
     public string KalaName { get; set; } = null!;
 
+    [Column("IDSanjesh")]
+    public int IdSanjesh { get; set; }
+
+    [Column("KalaType")]
+    public int KalaType { get; set; }
+
     [Column("MabFrosh")]
     public decimal MabFrosh { get; set; }
 
@@ -23,4 +29,16 @@ public class Kala
 
     [Column("IsDisabled")]
     public bool IsDisabled { get; set; }
+
+    [Column("IDAnbarFrosh")]
+    public int IdAnbarFrosh { get; set; }
+
+    [Column("MinCount")]
+    public int MinCount { get; set; }
+
+    [Column("IDSanjesh2")]
+    public int IdSanjesh2 { get; set; }
+
+    [Column("Quantity")]
+    public double Quantity { get; set; }
 }
