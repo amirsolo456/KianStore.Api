ee77e30bf98abf9b111bd5af20839f94f73fa39b
Amir <amirsol456@gmail.com>
2026-08-30 03:30:08 +0330

    Add customer search repository

--- Repositories/Interfaces/ICustomerRepository.cs
+++ Repositories/Interfaces/ICustomerRepository.cs
@@ -6,6 +6,7 @@ public interface ICustomerRepository
 {
     Task<Taraf?> GetByMobileAsync(string mobile);
     Task<Taraf?> GetByIdAsync(int id);
+    Task<List<Taraf>> SearchAsync(string search, int page = 1, int pageSize = 50);
     Task<Taraf> CreateAsync(Taraf taraf);
     Task<bool> ExistsByMobileAsync(string mobile);
     Task<int> GetNextIdAsync();
