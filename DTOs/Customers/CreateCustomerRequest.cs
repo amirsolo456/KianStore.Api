7ca975679e4a4ab4f07eaaeb738113f3cbe9d4b5
Amir <amirsol456@gmail.com>
2026-09-03 19:01:48 +0330

    Align customer creation request with Flutter person form

--- DTOs/Customers/CreateCustomerRequest.cs
+++ DTOs/Customers/CreateCustomerRequest.cs
@@ -2,8 +2,11 @@ namespace KianStore.Api.DTOs.Customers;
 
 public class CreateCustomerRequest
 {
-    public string FirstName { get; set; } = null!;
-    public string LastName { get; set; } = null!;
+    public int PersonType { get; set; } = 1;
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? CompanyName { get; set; }
     public string Mobile { get; set; } = null!;
+    public string? Phone { get; set; }
     public string? Address { get; set; }
 }
