b25b594bb72ffcb7837be6f9da9d128d81c818fc
Amir <amirsol456@gmail.com>
2026-09-03 19:06:54 +0330

    Fix local mobile API connection by disabling HTTPS redirect in development

--- Program.cs
+++ Program.cs
@@ -37,8 +37,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+else
+{
+    app.UseHttpsRedirection();
+}
 
-app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
 
