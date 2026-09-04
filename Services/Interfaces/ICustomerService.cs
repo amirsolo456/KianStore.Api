2dc1da98cc5d362d3f0500486c32ceb683489bda
Amir <amirsol456@gmail.com>
2026-09-04 17:28:47 +0330

    Keep customer service contract backward compatible

--- Services/Interfaces/ICustomerService.cs
+++ Services/Interfaces/ICustomerService.cs
@@ -5,7 +5,6 @@ namespace KianStore.Api.Services.Interfaces;
 
 public interface ICustomerService
 {
-    Task<ApiResponse<CustomerResponse>> GetByIdAsync(int id);
     Task<ApiResponse<CustomerResponse>> GetByMobileAsync(string mobile);
     Task<ApiResponse<IEnumerable<CustomerResponse>>> SearchAsync(string search, int page = 1, int pageSize = 50);
     Task<ApiResponse<CustomerResponse>> CreateCustomerAsync(CreateCustomerRequest request);
