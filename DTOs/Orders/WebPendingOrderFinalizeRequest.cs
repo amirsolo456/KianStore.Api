5e0b0b250c41d2f3550711b958833c0da8ccb55a
Amir <amirsol456@gmail.com>
2026-09-11 05:21:35 -0700

    Add pending website order finalization DTO

---
+++ DTOs/Orders/WebPendingOrderFinalizeRequest.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace KianStore.Api.DTOs.Orders;
+
+public sealed class WebPendingOrderFinalizeRequest
+{
+    [Range(1, int.MaxValue)] public int IdSal { get; init; } = 1405;
+    [Range(1, int.MaxValue)] public int IdAnbar { get; init; } = 1;
+    [Range(1, int.MaxValue)] public int IdMasool { get; init; } = 101;
+    [Range(0, int.MaxValue)] public int IdSandogh { get; init; }
+    [Range(0, int.MaxValue)] public int IdSandoghType { get; init; }
+    [Range(1, int.MaxValue)] public int SanadType { get; init; } = 12;
+    [StringLength(10)] public string? SabtDate { get; init; }
+    [StringLength(200)] public string? Des { get; init; }
+    [StringLength(700)] public string? Sharh { get; init; }
+    public bool CheckStock { get; init; } = true;
+    [MinLength(1)] public List<WebPendingOrderItemFinalizeRequest> Items { get; init; } = new();
+}
+
+public sealed class WebPendingOrderItemFinalizeRequest
+{
+    [Required, StringLength(20)] public string KalaId { get; init; } = null!;
+    [Range(typeof(decimal), "0.001", "79228162514264337593543950335")]
+    public decimal PurchasePrice { get; init; }
+}
