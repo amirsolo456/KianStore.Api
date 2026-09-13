cb0959724f9beb16f207b991c76a1c9e3ef6fc2e
Amir <amirsol456@gmail.com>
2026-09-13 10:53:14 -0700

    Add document mutation service contract

---
+++ Services/Interfaces/IDocumentMutationService.cs
@@ -0,0 +1,20 @@
+using KianStore.Api.Common;
+using KianStore.Api.DTOs.Documents;
+
+namespace KianStore.Api.Services.Interfaces;
+
+public interface IDocumentMutationService
+{
+    Task<ApiResponse<DocumentResponse>> UpdatePartnerSaleAsync(
+        int idSal,
+        string id,
+        CreateDocumentRequest request,
+        int? currentUserId,
+        CancellationToken cancellationToken = default);
+
+    Task<ApiResponse<DocumentResponse>> DeletePartnerSaleAsync(
+        int idSal,
+        string id,
+        int? currentUserId,
+        CancellationToken cancellationToken = default);
+}
