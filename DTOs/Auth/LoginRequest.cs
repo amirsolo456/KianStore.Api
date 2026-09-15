32ce6d5ad6f90afe28fefe42f17cbafdc5969591
Amir <amirsol456@gmail.com>
2026-09-15 04:24:35 -0700

    Add mobile login request

---
+++ DTOs/Auth/LoginRequest.cs
@@ -0,0 +1,7 @@
+namespace KianStore.Api.DTOs.Auth;
+
+public sealed class LoginRequest
+{
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
